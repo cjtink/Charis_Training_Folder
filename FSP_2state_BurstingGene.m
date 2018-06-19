3b8f8e695eb8d90fd373aa44f0fdb38b127ede78
cjtink <40178989+cjtink@users.noreply.github.com>
2018-06-19 11:05:35 -0600

    Attempt to Vectorize
    I need help with fully vectorizing this script. I am getting lost in what is considered vectorized and what is not and how to use it in the script.

--- FSP_2state_BurstingGene.m
+++ FSP_2state_BurstingGene.m
@@ -1,7 +1,7 @@
 function [P_fsp] = FSP_2state_BurstingGene(alpha,n)
 
 % Bursting gene model from previous SSA trainging script
-tstop = 100; x0 = [0;0]; t0 = 0; S = [-1,1,0,0,0;0,0,1,-1,1];
+tstop = 100; x0 = [0;0]; t0 = 0; S = [-1,1,0,0;0,0,1,-1];
 % alpha = 100; % This is a parameter to easily play around with the on/off switch
 koff = .3*alpha; kon = .1*alpha; gamma = 1; kr = 100;
 W0 = [0; kon; 0; 0]; W1 = [koff, 0; -kon, 0; kr, 0; 0, gamma];
@@ -10,23 +10,34 @@ W0 = [0; kon; 0; 0]; W1 = [koff, 0; -kon, 0; kr, 0; 0, gamma];
 W = @(x) W1*x + W0;
 
 A = spalloc(n+1,n+1,(2*(n+1)));
-for j = 1:2*(n+1)
-    % My addition:
-    x_j = index2state(j, n);
-    W_j = W(x_j);
-    A(j,j) = -sum(W_j);
-    
-%     A(j,j) = -sum(W(x(j)));
-    for r = 1:4
-        k = state2index(x_j+S(:,r),n);
-        if k > 0
-            i = k;
-            A(i, j) = W_j(r);
-            
-            %             A(i,j) = W(r(x_j));
-        end
+r = 1:4; j = 1:2*(n+1);
+x_j = index2state(j,n);
+x_j2 = reshape(x_j,[10,2])' % This is the correct form of the x_j matrix, use for future calculations
+
+% S_r = S(:,r)
+for dummy1 = j %trying to get a matrix out of this for loop, but I can't seem to figure out how to use it.
+    for dummy2 = r
+    k(dummy1,dummy2) = state2index(x_j2(:,dummy1)+S(:,dummy2),n)
     end
 end
+
+
+W_j = W(x_j2);
+v_diag = -sum(W_j); % This is correct for the diagonal calculation.
+
+
+% for j = 1:2*(n+1)
+%     x_j = index2state(j, n)
+%     W_j = W(x_j)
+%     A(j,j) = -sum(W_j)
+%     for r = 1:4
+%         k = state2index(x_j+S(:,r),n);
+%         if k > 0
+%             i = k;
+%             A(i, j) = W_j(r);
+%         end
+%     end
+% end
 
 
 
@@ -34,7 +45,6 @@ ode_system = @(t,p) A*p;
 
 tspan = [0 tstop]; 
 
-% ICs = x0; This initial condition will yield error
 
 index_x0 = state2index(x0, n);
 ICs = zeros(size(A,1), 1);
@@ -51,9 +61,4 @@ z = reshape(P,n+1,2);
 Z = sum(z,2);
 
 P_fsp = Z;
-
-% figure(3)
-% hold on
-% plot(0:n,Z)
-
 end
